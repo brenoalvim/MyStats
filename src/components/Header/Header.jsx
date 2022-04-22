8d940440b2324e857f64d5b5f7432bfd8c8055e0
Breno Alvim <alvimbrenosilva6@gmail.com>
2022-04-22 12:07:03 -0300

    new function with useEffect

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,17 +1,35 @@
 import './Header.css';
 
+import { useEffect } from 'react';
+
 function Header(props) {
+
+    useEffect(() => {
+        let sessionUser = sessionStorage.getItem('username');
+        if (sessionUser) {
+            getStatsGitHub(sessionUser);
+            window.sessionStorage.setItem("username", sessionUser);
+        } else {
+            getStatsGitHub("brenoalvim");
+            window.sessionStorage.setItem("username", "brenoalvim");
+        }
+    }, []);
 
     async function getStatsGitHub(username) {
         let response = await fetch(`https://api.github.com/users/${username}`);
         let responseJson = await response.json();
         props.getStats(responseJson);
-        document.querySelector(".form-control").value = ``;
-        sessionStorage.setItem('username', document.querySelector(".form-control").value);
+        let value = document.querySelector(".form-control").value;
+        window.sessionStorage.setItem("username", value);
     }
 
     function getName() {
         getStatsGitHub(document.querySelector(".form-control").value);
+    }
+
+    function removeUser() {
+        document.querySelector(".form-control").value = "";
+        window.sessionStorage.removeItem("username");
     }
     return (
         <div className="Header">
@@ -19,6 +37,9 @@ function Header(props) {
                 <input list="list-Users" type="text" class="form-control" aria-label="Text input with segmented dropdown button" placeholder={props.placeholder} />
                 <button title='search user' type="button" class="btn btn-outline-secondary" onClick={getName}>
                     <i class="fas fa-search"></i>
+                </button>
+                <button title='search user' type="button" class="btn btn-outline-secondary" onClick={removeUser}>
+                    <i class="fa-solid fa-delete-left"></i>
                 </button>
             </div>
             <datalist id="list-Users">
