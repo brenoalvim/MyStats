2bf4ca9c24c5054d0c5b46766d6e59e8ec7e00b7
Breno Alvim <alvimbrenosilva6@gmail.com>
2022-06-02 17:18:20 -0300

    fix bug

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,32 +1,40 @@
-import './Footer.css';
+import "./Footer.css";
 
 function Footer(props) {
-    return (
-        <div className="Footer" >
-            <a href={`https://github.com/${props.social}/MyStats`}
-                target="_blank"
-                rel="noopener noreferrer"
-                title="Access Source Code">
-                <i class="fa-brands fa-github"></i>
-            </a>
-            <a href={`https://linkedin.com/in/${props.social}`}
-                target="_blank"
-                rel="noopener noreferrer"
-                title="Creator's LinkedIn Profile">
-                <i class="fa-brands fa-linkedin-in"></i>
-            </a>
-            <a href={`mailto:${props.email}`}
-                target="_blank"
-                rel="noopener noreferrer"
-                title="send email to creator">
-                <i class="fa-solid fa-envelope"></i>
-            </a>
-            <div className="hr"></div>
-            <img src={props.linkFont}
-                title={`Developed by: ${props.developer}`}
-                alt={`Developed by: ${props.developer}`} />
-        </div>
-    );
+  return (
+    <div className="Footer">
+      <a
+        href={`https://github.com/${props.social}/MyStats`}
+        target="_blank"
+        rel="noopener noreferrer"
+        title="Access Source Code"
+      >
+        <i class="fa-brands fa-github"></i>
+      </a>
+      <a
+        href={`https://linkedin.com/in/${props.social}`}
+        target="_blank"
+        rel="noopener noreferrer"
+        title="Creator's LinkedIn Profile"
+      >
+        <i class="fa-brands fa-linkedin-in"></i>
+      </a>
+      <a
+        href={`mailto:${props.email}`}
+        target="_blank"
+        rel="noopener noreferrer"
+        title="send email to creator"
+      >
+        <i class="fa-solid fa-envelope"></i>
+      </a>
+      <div className="hr"></div>
+      <img
+        src={props.linkFont}
+        title={`Developed by: ${props.developer}`}
+        alt={`Developed by: ${props.developer}`}
+      />
+    </div>
+  );
 }
 
 export default Footer;
